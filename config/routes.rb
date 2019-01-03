6108f8bd0da4ad499e2e72e3e9564c76fe8b81ba
aschemily <aschemily@gmail.com>
2019-01-03 14:11:17 -0500

    styling form

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   get '/occupied', to:'parkingspots#occupied'
   get 'park/:id/status', to:'parkingspots#status', as:'status'
 
-  get '/', to:'static#index'
+  get '/', to:'static#index', as:'home'
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
